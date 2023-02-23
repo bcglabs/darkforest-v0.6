1119f414d4f526ee0f63b57245b2fc2f4ba72af0
UDADU <udadu695838885@gmail.com>
2023-02-23 21:38:52 +0800

    中文

--- client/src/Frontend/Panes/TooltipPanes.tsx
+++ client/src/Frontend/Panes/TooltipPanes.tsx
@@ -8,11 +8,11 @@ import { useAccount, useSelectedPlanet, useUIManager } from '../Utils/AppHooks';
 export function NetworkHealthPane() {
   return (
     <>
-      <White>xDAI Tx Speed: </White>For each auto gas setting (which you can choose in the{' '}
-      <White>Settings</White> Pane), the average amount of time it takes a transaction with that
-      setting to confirm. The Dark Forest client uploads diagnostic info (you can turn this off via
-      settings), which is aggregated into this network health indicator. I hope you find it helpful
-      in cases the network is being slow.
+      <White>xDAI 发送速度： </White>对于每个自动气体设置（您可以在{' '}
+      <White>设置</White> 窗格），与该窗格进行交易所需的平均时间
+      设置来确认。黑暗森林客户端上传诊断信息（你可以通过
+      设置），它被聚合到这个网络健康指标中。我希望你觉得这对你有帮助
+      在网络缓慢的情况下。
     </>
   );
 }
@@ -20,57 +20,57 @@ export function NetworkHealthPane() {
 export function WithdrawSilverButton() {
   return (
     <>
-      This is a <Text>Spacetime Rip</Text> where you can withdraw <SilverLabel /> for <ScoreLabel />
+      这是一个 <Text>时空撕裂</Text> 你可以在那里取款 <SilverLabel /> 为了 <ScoreLabel />
       !
     </>
   );
 }
 export function DefenseMultiplierPane() {
-  return <>Defense multiplier</>;
+  return <>防御倍数</>;
 }
 
 export function EnergyCapMultiplierPane() {
-  return <>EnergyCap multiplier</>;
+  return <>EnergyCap乘数</>;
 }
 
 export function EnergyGrowthMultiplierPane() {
-  return <>EnergyGrowth multiplier</>;
+  return <>能源增长乘数</>;
 }
 
 export function RangeMultiplierPane() {
-  return <>Range multiplier</>;
+  return <>范围乘数</>;
 }
 
 export function SpeedMultiplierPane() {
-  return <>Speed multiplier</>;
+  return <>速度倍增器</>;
 }
 
 export function DepositArtifactPane() {
-  return <>Deposit this artifact</>;
+  return <>存入此神器</>;
 }
 
 export function DeactivateArtifactPane() {
-  return <>Deactivate this artifact</>;
+  return <>停用此工件</>;
 }
 
 export function WithdrawArtifactPane() {
-  return <>Withdraw this artifact</>;
+  return <>撤回这个神器</>;
 }
 
 export function ActivateArtifactPane() {
-  return <>Activate this artifact</>;
+  return <>激活这个神器</>;
 }
 
 export function TimeUntilActivationPossiblePane() {
-  return <>You must wait this amount of time before you can activate this artifact</>;
+  return <>您必须等待这段时间才能激活此神器</>;
 }
 
 export function TwitterHandleTooltipPane() {
   return (
     <>
-      You may connect your account to <White>Twitter</White>
+      您可以将您的帐户连接到 <White>推特</White>
       <br />
-      to identify yourself on the <White>Leaderboard</White>.
+      在 <White>排行榜</White>.
     </>
   );
 }
@@ -78,7 +78,7 @@ export function TwitterHandleTooltipPane() {
 export function RankTooltipPane() {
   return (
     <>
-      Your current rank, based on <ScoreLabel />.
+      您当前的排名，基于 <ScoreLabel />.
     </>
   );
 }
@@ -86,8 +86,8 @@ export function RankTooltipPane() {
 export function ScoreTooltipPane() {
   return (
     <>
-      You earn <ScoreLabel /> by finding artifacts and withdrawing silver. Check out the{' '}
-      <White>Help Pane</White> for more info on scoring.
+      你赚 <ScoreLabel /> 通过寻找文物和提取白银。查看{' '}
+      <White>帮助面板</White> 有关评分的更多信息。
     </>
   );
 }
@@ -95,7 +95,7 @@ export function ScoreTooltipPane() {
 export function MiningPauseTooltipPane() {
   return (
     <>
-      Start / Stop your <White>explorer</White>. Your explorer looks for planets in chunks of{' '}
+      开始/停止你的<White>探险家</White>. 你的探险家寻找大块的行星{' '}
       <White>16</White> x <White>16</White>.
     </>
   );
@@ -104,9 +104,9 @@ export function MiningPauseTooltipPane() {
 export function MiningTargetTooltipPane() {
   return (
     <>
-      Change the location of your <White>explorer</White>. Click anywhere on the{' '}
-      <White>Game Screen</White>, and your <White>miner</White> will start hashing around that
-      chunk.
+      改变你的位置<White>探险家</White>. 单击上的任意位置{' '}
+      <White>游戏画面</White>, 和你的 <White>矿工</White> 将开始围绕它散列
+      块。
     </>
   );
 }
@@ -114,7 +114,7 @@ export function MiningTargetTooltipPane() {
 export function CurrentMiningTooltipPane() {
   return (
     <>
-      The current coordinates of your <White>explorer</White>.
+      你的当前坐标 <White>探险家</White>.
     </>
   );
 }
@@ -122,7 +122,7 @@ export function CurrentMiningTooltipPane() {
 export function BonusTooltipPane() {
   return (
     <>
-      <Green>This stat has been randomly doubled!</Green>
+      <Green>此统计数据已随机翻倍！</Green>
     </>
   );
 }
@@ -130,8 +130,8 @@ export function BonusTooltipPane() {
 export function SilverTooltipPane() {
   return (
     <>
-      <White>Silver:</White> the universe's monetary resource. It allows you to buy upgrades. Only
-      <White> Asteroid Fields</White> produce silver or so we've been told...
+      <White>银:</White> 宇宙的货币资源。它允许您购买升级。仅有的
+      <White> 小行星带</White> 生产银或者我们被告知......
     </>
   );
 }
@@ -139,8 +139,8 @@ export function SilverTooltipPane() {
 export function EnergyTooltipPane() {
   return (
     <>
-      <White>Energy:</White> allows you to make moves. Energy grows following an{' '}
-      <White>s-curve</White>, and grows fastest at <White>50% capacity</White>.
+      <White>活力:</White> 允许你做出动作。能量增长如下{' '}
+      <White>s曲线</White>, 并且增长最快 <White>50% 容量</White>.
     </>
   );
 }
@@ -151,7 +151,7 @@ export function PlanetRankTooltipPane() {
   const rank = getPlanetRank(selected.value);
   return (
     <>
-      This planet is <White>{isFullRank(selected.value) ? 'fully upgraded' : 'rank ' + rank}</White>
+      这个星球是 <White>{isFullRank(selected.value) ? '全面升级' : '排名 ' + rank}</White>
       !
     </>
   );
@@ -160,8 +160,8 @@ export function PlanetRankTooltipPane() {
 export function MaxLevelTooltipPane() {
   return (
     <>
-      This planet is <White>Level 9</White>, making it one of the <br />
-      most powerful planets in the galaxy!
+      这个星球是 <White>等级 9</White>, 使其成为其中之一 <br />
+      银河系中最强大的行星！
     </>
   );
 }
@@ -169,7 +169,7 @@ export function MaxLevelTooltipPane() {
 export function SilverProdTooltipPane() {
   return (
     <>
-      This planet produces <White>Silver</White>! Use it to buy upgrades!
+      这个星球产生 <White>银</White>! 用它来购买升级！
     </>
   );
 }
@@ -183,26 +183,26 @@ export function SelectedSilverTooltipPane() {
       {selected.value ? (
         <>
           <>
-            Silver:
+            银:
             <span>{selected.value.silver}</span>
           </>
           <>
-            Cap:
+            上限:
             <span>{selected.value.silverCap}</span>
           </>
           {selected.value.planetType === PlanetType.SILVER_MINE ? (
             <>
-              Growth:
+              生长:
               <span>{selected.value.silverGrowth * 60}</span>
             </>
           ) : (
             <>
-              <Red>This planet does not produce silver.</Red>
+              <Red>这个星球不产银.</Red>
             </>
           )}
         </>
       ) : (
-        <>Select a planet to view more about its stats. </>
+        <>选择一颗行星以查看有关其统计数据的更多信息. </>
       )}
     </>
   );
@@ -211,9 +211,9 @@ export function SelectedSilverTooltipPane() {
 export function RangeTooltipPane() {
   return (
     <>
-      <White>Range:</White> how far you can send your forces. <White>Forces decay</White> the
-      farther out you send them. <br />
-      Higher range means that you can send forces the same distance with less decay.
+      <White>范围:</White> 你可以派遣你的部队多远. <White>力量衰减</White> 这
+      你把他们送到更远的地方。 <br />
+      更高的范围意味着您可以以更少的衰减发送相同的距离。
     </>
   );
 }
@@ -221,8 +221,8 @@ export function RangeTooltipPane() {
 export function MinEnergyTooltipPane() {
   return (
     <>
-      The minimum energy you need to send a move from this planet. <br />
-      Moves incur a base cost of 5% of the planet's <White>Energy Cap</White>.
+      从这个星球发出动作所需的最小能量。 <br />
+      移动产生的基本成本是地球的 5%<White>能量上限</White>.
     </>
   );
 }
@@ -230,7 +230,7 @@ export function MinEnergyTooltipPane() {
 export function Time50TooltipPane() {
   return (
     <>
-      Time to <White>50%</White> of full energy.
+      时间到 <White>50%</White>充满活力.
     </>
   );
 }
@@ -238,8 +238,8 @@ export function Time50TooltipPane() {
 export function Time90TooltipPane() {
   return (
     <>
-      Time to <White>90%</White> of full energy. Since energy grows on an s-curve, energy growth
-      slows drastically by this point.
+      时间到 <White>90%</White> 满满的能量。由于能量在 S 曲线上增长，能量增长
+      在这一点上急剧放缓。
     </>
   );
 }
@@ -247,8 +247,8 @@ export function Time90TooltipPane() {
 export function EnergyGrowthTooltipPane() {
   return (
     <>
-      <White>Energy Growth:</White> the maximum growth rate of this planet's energy representing the
-      rate at the middle of the <White>s-curve</White>.
+      <White>能量增长:</White> 这个星球的能量的最大增长率代表
+      率在中间<White>s曲线</White>.
     </>
   );
 }
@@ -256,7 +256,7 @@ export function EnergyGrowthTooltipPane() {
 export function SilverGrowthTooltipPane() {
   return (
     <>
-      <White>Silver Growth</White>: the per-minute linear growth rate of this planet's silver.
+      <White>银增长</White>: 这个星球的白银每分钟线性增长率。
     </>
   );
 }
@@ -264,7 +264,7 @@ export function SilverGrowthTooltipPane() {
 export function SilverCapTooltipPane() {
   return (
     <>
-      <White>Silver Cap</White>: the maximum silver that this planet can hold.
+      <White>银上限</White>: 这个星球可以容纳的最大白银。
     </>
   );
 }
@@ -272,7 +272,7 @@ export function SilverCapTooltipPane() {
 export function PiratesTooltipPane() {
   return (
     <>
-      <Red>This planet has space pirates!</Red> Move energy to unoccupied planets to conquer them!
+      <Red>这个星球有宇宙海盗！</Red> 将能量转移到无人居住的行星上以征服它们！
     </>
   );
 }
@@ -280,44 +280,44 @@ export function PiratesTooltipPane() {
 export function UpgradesTooltipPane() {
   return (
     <>
-      <White>Planet Rank</White>: the number of times you've upgraded your planet.
+      <White>星球等级</White>: 您升级星球的次数。
     </>
   );
 }
 
 export function ModalHelpTooltipPane() {
-  return <>View patch notes and instructions</>;
+  return <>查看补丁说明和说明</>;
 }
 
 export function ModalPlanetDetailsTooltipPane() {
-  return <>View detailed information about the selected planet</>;
+  return <>查看所选星球的详细信息</>;
 }
 
 export function ModalLeaderboardTooltipPane() {
-  return <>View the top players, and their top planets</>;
+  return <>查看顶级玩家和他们的顶级行星</>;
 }
 
 export function ModalPlanetDexTooltipPane() {
-  return <>View a list of your planets</>;
+  return <>查看您的行星列表</>;
 }
 
 export function ModalUpgradeDetailsTooltipPane() {
-  return <>Upgrade the selected planet</>;
+  return <>升级选定的星球</>;
 }
 
 export function ModalTwitterVerificationTooltipPane() {
-  return <>Connect your address to Twitter</>;
+  return <>将您的地址连接到 Twitter</>;
 }
 
 export function ModalBroadcastTooltipPane() {
-  return <>Broadcast the selected planet's coordinates to the world</>;
+  return <>向全世界广播所选行星的坐标</>;
 }
 
 export function BonusEnergyCapTooltipPane() {
   return (
     <>
       <Green>
-        This planet's <White>Energy Cap</White> has been randomly doubled!
+        这个星球的<White>能量上限</White> 已经随机翻倍了！
       </Green>
     </>
   );
@@ -327,7 +327,7 @@ export function BonusEnergyGroTooltipPane() {
   return (
     <>
       <Green>
-        This planet's <White>Energy Growth</White> has been randomly doubled!
+        这个星球的<White>能量增长</White> 已经随机翻倍了！
       </Green>
     </>
   );
@@ -337,7 +337,7 @@ export function BonusRangeTooltipPane() {
   return (
     <>
       <Green>
-        This planet's <White>Range</White> has been randomly doubled!
+        这个星球的<White>范围</White> 已经随机翻倍了！
       </Green>
     </>
   );
@@ -347,7 +347,7 @@ export function BonusSpeedTooltipPane() {
   return (
     <>
       <Green>
-        This planet's <White>Speed</White> has been randomly doubled!
+        这个星球的<White>速度</White> 已经随机翻倍了！
       </Green>
     </>
   );
@@ -357,7 +357,7 @@ export function BonusDefenseTooltipPane() {
   return (
     <>
       <Green>
-        This planet's <White>Defense</White> has been randomly doubled!
+        这个星球的 <White>防御</White> 已经随机翻倍了！
       </Green>
     </>
   );
@@ -367,7 +367,7 @@ export function BonusSpaceJunkTooltipPane() {
   return (
     <>
       <Green>
-        This planet's <White>Space Junk</White> has been randomly halved!
+        这个星球的 <White>宇宙垃圾</White> 已经随机减半了！
       </Green>
     </>
   );
@@ -382,8 +382,8 @@ export function ClowntownTooltipPane() {
     <>
       <span>
         {selected.value?.owner === account
-          ? `You are the proud mayor of Clown Town!`
-          : `It's a town of clowns...`}
+          ? `你是小丑镇骄傲的市长！`
+          : `这是一个小丑之城...`}
       </span>
     </>
   );
@@ -392,8 +392,8 @@ export function ClowntownTooltipPane() {
 function DefenseTooltipPane() {
   return (
     <>
-      <White>Defense:</White> Planets with higher defense will negate incoming damage. Planets with
-      lower than 100 defense are vulnerable and will take more damage!
+      <White>防御:</White> 具有更高防御力的行星将抵消即将到来的伤害。行星与
+      低于 100 的防御是脆弱的，并且会受到更多的伤害！
     </>
   );
 }
@@ -401,10 +401,10 @@ function DefenseTooltipPane() {
 function SpaceJunkTooltipPane() {
   return (
     <>
-      <White>Space Junk:</White> Planets are all filled with junk! Sending energy to a planet with
-      junk will remove the junk from that planet and add it to your total junk. Once you reach your
-      junk limit, you will not be able to capture planets that have junk. Abandoning planets will
-      reduce your space junk and place it back on the planet.
+      <White>宇宙垃圾:</White> 行星上到处都是垃圾！向行星发送能量
+      junk 将从那个星球上删除垃圾并将其添加到您的总垃圾中。一旦你达到你的
+      垃圾限制，您将无法捕获有垃圾的行星。放弃行星将
+      减少你的太空垃圾并将其放回地球。
     </>
   );
 }
@@ -416,12 +416,12 @@ function AbandonTooltipPane() {
 
   return (
     <>
-      <Red>Abandon your planet:</Red> Give up ownership of this planet to dump some of your space
-      junk here. This triggers a special movement that sends full <White>Energy/Silver</White> and
-      gives a <Green>Range boost of {abandonRangeBoost}x</Green> and a{' '}
-      <Green>Speed boost of {abandonSpeedBoost}x</Green>.
+      <Red>放弃你的星球：</Red> 放弃这个星球的所有权来倾倒你的一些空间
+      垃圾在这里。这会触发一个特殊的动作，发送完整的 <White>能量/银</White> 和
+      给出一个 <Green>射程提升 {abandonRangeBoost}x</Green> 和一个{' '}
+      <Green>速度提升 {abandonSpeedBoost}x</Green>.
       <br />
-      <Red>You cannot abandon your home planet, or a planet that has incoming voyages.</Red>
+      <Red>你不能放弃你的家乡星球，或者一个有即将到来的航行的星球。</Red>
     </>
   );
 }
@@ -429,61 +429,61 @@ function AbandonTooltipPane() {
 function SpeedTooltipPane() {
   return (
     <>
-      <White>Speed:</White> The rate at which energy travels across the universe, the faster the
-      better!
+      <White>速度:</White> 能量在宇宙中传播的速度越快
+      更好的！
     </>
   );
 }
 
 function RetryTransactionPane() {
-  return <>Retry transaction.</>;
+  return <>重试事务。</>;
 }
 
 function CancelTransactionPane() {
-  return <>Cancel transaction.</>;
+  return <>取消交易。</>;
 }
 
 function PrioritizeTransactionPane() {
-  return <>Prioritize transaction.</>;
+  return <>优先交易。</>;
 }
 
 function ArtifactBuffPane() {
-  return <>A powerful artifact on this planet is influencing this stat!</>;
+  return <>这个星球上一个强大的神器正在影响这个数据！</>;
 }
 
 function PluginsTooltipPane() {
-  return <>Manage plugins, which allow you to add functionality to the client.</>;
+  return <>管理插件，它允许您向客户端添加功能。</>;
 }
 
 function SettingsPane() {
-  return <>Manage settings - export SKEY, manage maps, and more.</>;
+  return <>管理设置 -导出 SKEY、管理地图等。</>;
 }
 
 function YourArtifacts() {
-  return <>View your artifacts.</>;
+  return <>查看您的工件。</>;
 }
 
 function InvadablePane() {
-  return <>This planet is in a scoring zone and can be invaded</>;
+  return <>这个星球处于得分区，可以被入侵</>;
 }
 
 function CapturablePane() {
-  return <>This planet has been invaded, which means you can capture it for score.</>;
+  return <>这个星球已被入侵，这意味着您可以占领它来得分。</>;
 }
 
-const ModalWithdrawSilverTooltipPane = () => <>Withdraw silver to earn score.</>;
+const ModalWithdrawSilverTooltipPane = () => <>提取银币以赚取分数。</>;
 
-const Hats = () => <>Buy hats for the selected planet.</>;
+const Hats = () => <>为选定的星球购买帽子。</>;
 
 const FindArtifact = () => (
   <>
-    <Green>This planet has a powerful artifact hidden somewhere!</Green> Maybe you could find it...
+    <Green>这颗星球的某处隐藏着一件强大的神器！</Green> 也许你能找到它...
   </>
 );
 
-const ArtifactStored = () => <>This planet has a powerful artifact on it!</>;
+const ArtifactStored = () => <>这颗星球上，有着强大的神器！</>;
 
-const HashesPerSec = () => <>hashes / sec</>;
+const HashesPerSec = () => <>哈希/秒</>;
 
 export function TooltipContent({ name }: { name: TooltipName | undefined }) {
   if (name === TooltipName.SilverGrowth) return <SilverGrowthTooltipPane />;
